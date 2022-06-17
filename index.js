ec2ca41d1858cd39f0c54275a622b1896090f8af
rickie maweu <rickiemaweu@gmail.com>
2022-06-17 21:33:34 +0300

    commit tree 9c20e74cfeaee9da552708ae465bfa42b28fe3bb

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //check if the word contains a palindrome
+   const palindrome = word.split("").reverse().join("");
+   const checkPalindrome=(palindrome===word) ? true : false;
+   return checkPalindrome;
 }
 
 /* 
   Add your pseudocode here
+  const palindrome=word.reverse
+  if (palindrome===word){
+    return true
+  }
+  else{
+    return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+  get the word from the function
+  reverse the word
+  compare the reversed word to the original word
+  if word is equal to the original returns true
+  else returns false
 */
 
 // You can run `node index.js` to view these console logs
